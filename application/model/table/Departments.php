a3851b524e2cce51e4650198f3677c57b8ab1d66
cody <nathancodymann@gmail.com>
2013-06-06 18:28:58 -0500

    ui

--- application/model/table/Departments.php
+++ application/model/table/Departments.php
@@ -2,4 +2,21 @@
 
 class Application_Model_Table_Departments extends Zend_Db_Table_Abstract{
 	protected $_name = 'departments';
+
+	/**
+	 * Return all departments from departments table
+	 *
+	 * @return Array all department data
+	 */
+
+	public function getAllDepartments() {
+
+		$query = $this->fetchAll();
+		$departments = array();
+
+		foreach ($query as $department) {
+			$departments[] = new Application_Model_Department($department);
+		}
+		return $departments;
+	}
 }
--- application/modules/admin/controllers/ContentController.php
+++ application/modules/admin/controllers/ContentController.php
@@ -6,7 +6,15 @@ class Admin_ContentController extends Zend_Controller_Action{
 
 	public function addAction(){}
 	
-	public function editAction(){}
+	public function editAction(){
+		$departments = new Application_Model_Table_Departments;
+		
+		$this->view->departments = $departments->getAllDepartments();
+	}
 	
-    public function indexAction(){}
+    public function indexAction(){
+    	$departments = new Application_Model_Table_departments;
+
+    	$this->view->departments = $departments->getAllDepartments();
+    }
 }
