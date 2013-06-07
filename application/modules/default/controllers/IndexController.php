7487ae66c4817e68eddd27124db95a6ed6eb04fb
cody <nathancodymann@gmail.com>
2013-06-06 22:48:48 -0500

    ui

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -2,5 +2,9 @@
 class IndexController extends Zend_Controller_Action{
     public function init(){}
 
-    public function indexAction(){}
+    public function indexAction(){
+    	$colleges = new Application_Model_Table_Colleges;
+
+    	$this->view->colleges = $colleges->fetchAll();
+    }
 }
